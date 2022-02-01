f9ede358b1f3f7a871544a02a3be0804b1ed5abc
tangnatt <97584438+tangnatt@users.noreply.github.com>
2022-02-01 16:07:32 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,9 +1,24 @@
 <html>
-  <head>
-    <title>งานที่ 1</title>
+  <head> 
+    <title>page 1</title> 
   </head>
-  <body>
-      ข้อความที่ 1 <br>
-      ข้อความที่ 2
-  </body>
-</html>
+  <body bgcolor="lightblue">
+    <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
+    <br> 
+    <b>ชื่อ :</b> <i>นางสาวณัฐสุดา เนาว์แก้ว</i>
+    <br> 
+    <b>ชื่อเล่น :</b> <i>ตังค์</i> 
+    <br>
+    <b>ชั้น :</b> <i>ม.4/12</i>
+    <br> 
+    <b>เลขที่ :</b> <i>16</i>
+    <br>
+    <b>เลขประจำตัวนักเรียน :</b> <i>57056</i>
+    <br>
+    <b>โรงเรียน :</b> <i>หล่มสักวิทยาคม<i> 
+    </body>
+    </html>
+
+
+    
+
