d4bf26b508a244ec7beb272b834458b13270cd0d
tangnatt <97584438+tangnatt@users.noreply.github.com>
2022-03-01 18:28:59 +0700

    Create font1.php

---
+++ font1.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Trirong:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Fahkwang:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&family=Trirong:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+      body {
+        font-size: 1.25rem;
+        text-align: center;
+      }
+      .charm {
+        font-family: 'Charm', cursive;
+      }
+      .pattaya {
+        font-family: 'Pattaya', sans-serif;
+      }
+    </style>
+  </head>
+ <body>
+    <h1 class="charm">ณัฐสุดา เนาว์แก้ว เลขที่ 16 ห้อง ม.4/12 เลขประจำตัว 57056</h1>
+    <h1 class="pattaya">ณัฐสุดา เนาว์แก้ว เลขที่ 16 ห้อง ม.4/12 เลขประจำตัว 57056</h1>
+  </body>
+</html>
