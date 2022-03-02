98f2004a697d76af965a438a073a926c5aac3eb1
tangnatt <97584438+tangnatt@users.noreply.github.com>
2022-03-02 13:37:54 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>อริสรา ทองบริสุทธิ์</td>
+        <td><img src="https://scontent.fbkk5-1.fna.fbcdn.net/v/t39.30808-6/274720138_1302006693632380_4600763225902350878_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=730e14&_nc_eui2=AeGFeroslPYkrI2vCyoAEGInbTukkoCa35RtO6SSgJrflKs6SAovWrgDensLLUgWXIbjtcRdBKQYS4U8si7LyHfG&_nc_ohc=Jjb1kdAGPzkAX8fGilU&_nc_ht=scontent.fbkk5-1.fna&oh=00_AT-x93oqr0Q2huc3ngXLWjjfKFjlIwp8IihmqzDPrpz9_g&oe=62235FE8"></td>
+        <td><a href="https://www.facebook.com/profile.php?id=100014691842081">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
